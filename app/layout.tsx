dbe3c5a4e132ac52fd6d054a1256f71208f0e39b
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-08-20 17:06:07 +0200

    em

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
-import "./globals.css";
-import Link from "next/link";
+import type { Metadata } from 'next';
+import { Inter, Poppins } from 'next/font/google';
+import './globals.css';
+import Link from 'next/link';
 
 const poppins = Poppins({
-  weight: ["400","500","600","700"],
-  variable: "--font-poppins",
-  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+  subsets: ['latin'],
 });
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
+  variable: '--font-inter',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "My portfolio",
+  title: 'Portfolio',
+  description: 'My portfolio',
 };
 
 export default function RootLayout({
@@ -24,32 +24,55 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-const navLinks = [
-  {href: "/", label: "Home"},
-  {href: "/projects", label: "Projects"},
-  {href: "/about", label: "About Me"},
-  {href: "/contact", label: "Contact"}
-]
+  const navLinks = [
+    { href: '/', label: 'Home' },
+    { href: '/projects', label: 'Projects' },
+    { href: '/about', label: 'About Me' },
+    { href: '/contact', label: 'Contact' },
+  ];
 
   return (
     <html lang="en">
       <body
-        className={"antialiased"}
-      >
-        <header>
-          <nav className="grid grid-flow-col text-2xl">
-            <Link href={"/"} className="p-4"><h1>Ali Zaifie</h1></Link>
-            <ul className="grid grid-flow-col justify-around">
-              {navLinks.map((link: {href: string, label: string}, index) => 
-              <li key={index} className="grid">
-                <Link className={`self-center text-center ${poppins.variable}`} href={link.href}>{link.label}</Link>
-              </li>
-              )}
+        className={
+          'antialiased min-h-dvh grid grid-rows-[auto_1fr_auto] md:grid-cols-[1fr_3fr] bg-gray-950 text-white'
+        }>
+        <header className="md:h-dvh md:sticky md:top-0 border-r-2 border-white-400">
+          <nav className="grid grid-flow-col md:grid-rows-5 md:min-h-dvh text-3xl">
+            <Link
+              href={'/'}
+              className="col-span-2 w-fit md:w-full md:text-left py-10 px-4 content-center">
+              <h1 className="text-center">Ali Zaifie</h1>
+            </Link>
+            <ul className="col-span-4 grid grid-flow-col md:row-span-4 md:grid-flow-row justify-around">
+              {navLinks.map((link: { href: string; label: string }, index) => (
+                <li key={index} className="grid">
+                  <Link
+                    className={`py-10 self-center text-center  mw:w-full md:text-left ${poppins.variable}`}
+                    href={link.href}>
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </nav>
         </header>
-        {children}
+        <div className="">{children}</div>
+        <footer>
+          <nav className="grid grid-auto-col md:hidden">
+            <ul className="col-span-4 grid grid-flow-col justify-around">
+              {navLinks.map((link: { href: string; label: string }, index) => (
+                <li key={index} className="grid">
+                  <Link
+                    className={`py-10 self-center text-center ${poppins.variable}`}
+                    href={link.href}>
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className="grid justify-center">
+      <h2 className="text-center text-4xl">This page is under construction!</h2>
+      <article className="text-center mt-10">
+        <h2 className="text-center text-2xl ">About me</h2>
+        <p className="max-w-4/6 mx-auto">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+          esse labore impedit obcaecati aut eum voluptates reiciendis iste
+          possimus veritatis, incidunt quisquam molestiae vero! Perspiciatis
+          officiis quod quisquam ex at!
+        </p>
+      </article>
+      <article className="text-center">
+        <h2 className="text-center text-2xl ">Latest projects</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+          esse labore impedit obcaecati aut eum voluptates reiciendis iste
+          possimus veritatis, incidunt quisquam molestiae vero! Perspiciatis
+          officiis quod quisquam ex at!
+        </p>
+      </article>
     </div>
   );
 }

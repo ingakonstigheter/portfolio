8ed30616cb527e79d2869749455cc8ad1a27fa92
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-12-06 21:01:13 +0100

    begining of full hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,24 @@
-import React from "react";
 import CodeWindow from "./code-window";
 
 export default function Hero() {
-  return <CodeWindow></CodeWindow>;
+  const name = "Ali Zaifie";
+  return (
+    <div className="grid gap-4 mt-4 lg:grid-cols-2 items-center">
+      <div>
+        <h1 className="text-6xl font-bold mb-8">
+          <span className="text-7xl font-extrabold block">Hello</span>
+          I'm
+          <span className="gradient-text"> {name}</span>
+        </h1>
+
+        <p className="text-2xl mb-6 max-w-3xl">
+          I'm a passionate full-stack developer with expertise in building
+          scalable web applications and solving complex problems. Explore my
+          portfolio to see my skills, experience, and projects.
+        </p>
+        <button className="btn-primary">Learn More</button>
+      </div>
+      <CodeWindow></CodeWindow>
+    </div>
+  );
 }
